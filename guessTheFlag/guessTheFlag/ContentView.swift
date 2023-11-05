d3447080aea968bea79fa7ade6ef6d81d9f45a9c
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-05 07:07:17 +0200

    init the project

---
+++ guessTheFlag/guessTheFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  guessTheFlag
+//
+//  Created by OLEKSANDR ISAIEV on 05.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ guessTheFlag/guessTheFlag/guessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheFlagApp.swift
+//  guessTheFlag
+//
+//  Created by OLEKSANDR ISAIEV on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct guessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
