8f949fc9a8a8e4925e7137dd2f8e75d223fafd97
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-07 06:47:10 +0200

    update ui settings

--- guessTheFlag/guessTheFlag/ContentView.swift
+++ guessTheFlag/guessTheFlag/ContentView.swift
@@ -13,17 +13,23 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        VStack(spacing: 30) {
-            VStack {
-                Text("Tap the flag of")
-                Text(countries[correctAnswer])
-            }
-            
-            ForEach(0..<3) { number in
-                Button {
-                    //
-                } label: {
-                    Image(countries[number])
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        //
+                    } label: {
+                        Image(countries[number])
+                    }
                 }
             }
         }
