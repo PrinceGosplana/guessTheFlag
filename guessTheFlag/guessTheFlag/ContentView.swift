78fbeff9f0824bb7e1741640bb32f46c88d0ba4d
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-07 06:43:14 +0200

    add main ui view elements

--- guessTheFlag/guessTheFlag/ContentView.swift
+++ guessTheFlag/guessTheFlag/ContentView.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        Text("Start")
+        VStack(spacing: 30) {
+            VStack {
+                Text("Tap the flag of")
+                Text(countries[correctAnswer])
+            }
+            
+            ForEach(0..<3) { number in
+                Button {
+                    //
+                } label: {
+                    Image(countries[number])
+                }
+            }
+        }
+        
     }
 }
 
