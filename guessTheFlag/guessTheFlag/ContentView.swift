e15107b0e885f3866b169608a2ca9ab4060d024e
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-12 16:18:40 +0200

    fix flags description

--- guessTheFlag/guessTheFlag/ContentView.swift
+++ guessTheFlag/guessTheFlag/ContentView.swift
@@ -23,9 +23,9 @@ struct ContentView: View {
         "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
         "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
         "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
-        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
         "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
         "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow",
         "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
     ]
     var body: some View {
